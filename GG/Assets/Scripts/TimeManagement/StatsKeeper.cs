86c99ddd7a6bc5f2015113e6b4a2877c2db800e5
Wavet <105194898+W4v37@users.noreply.github.com>
2023-04-23 22:36:33 +0200

    TimeTest
    Battle
    Win/Lose

--- GG/Assets/Scripts/TimeManagement/StatsKeeper.cs
+++ GG/Assets/Scripts/TimeManagement/StatsKeeper.cs
@@ -5,24 +5,29 @@ using UnityEngine.UI;
 
 public enum Activity
 {
-    Idle, Training, Healing, Diging, Sleeping
+    Idle, Training, Healing, Diging, Sleeping, Fighting
 }
 
 public class StatsKeeper : MonoBehaviour
 {
     private int DAY_START = 8;
     private int DAY_END = 18;
-    private static float TIME_SPEED_ACTIVITY = 600;
-    private static float TIME_SPEED_NIGHT = 3600;
+    private static float TIME_SPEED_ACTIVITY = 1800;
+    private static float TIME_SPEED_NIGHT = 36000;
 
     public Text stats;
     public Image display;
     public TimeKeeper timeKeeper;
     public Sprite[] sprites;
 
-    private float health = 50;
+    private float health = 500;
     private float strength = 1;
     private float tunnel = 0;
+
+    private float enemyHealth;
+    private float enemyStrength;
+    private float attackTimer;
+    private bool turn = true;
 
     private int skipStart = 0;
 
@@ -31,6 +36,8 @@ public class StatsKeeper : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
+        if (timeKeeper != null)
+            timeKeeper.TimePerSecond = TIME_SPEED_ACTIVITY;
     }
 
     // Update is called once per frame
@@ -44,7 +51,7 @@ public class StatsKeeper : MonoBehaviour
             }
             else
             {
-                float statChange = Time.deltaTime * 0.1f;
+                float statChange = Time.deltaTime;
 
                 switch (currentActivity)
                 {
@@ -53,19 +60,49 @@ public class StatsKeeper : MonoBehaviour
                         break;
 
                     case Activity.Healing:
-                        health += statChange;
+                        health += statChange * 15;
                         break;
 
                     case Activity.Diging:
                         if (health > 1)
                         {
-                            statChange *= 5;
-
-                            health -= statChange;
-                            tunnel += statChange;
+                            health -= statChange * 3.5f;
+                            tunnel += statChange * 2;
                         }
                         else
                             ChangeActivity(Activity.Idle);
+                        break;
+
+                    case Activity.Fighting:
+                        if (health > 0)
+                        {
+                            //Fight
+                            if (enemyHealth > 0)
+                            {
+                                if (attackTimer > 0)
+                                    attackTimer -= Time.deltaTime;
+                                else
+                                {
+                                    if (turn)
+                                        enemyHealth -= strength;
+                                    else
+                                        health -= enemyStrength;
+
+                                    turn = !turn;
+                                    attackTimer = 1;
+                                }
+                            }
+                            //Win
+                            else
+                            {
+                                SkipDay();
+                            }
+                        }
+                        //Lose
+                        else
+                        {
+
+                        }
                         break;
                 }
             }
@@ -77,15 +114,42 @@ public class StatsKeeper : MonoBehaviour
                 currentActivity = Activity.Idle;
                 timeKeeper.TimePerSecond = TIME_SPEED_ACTIVITY;
                 timeKeeper.Stopped = true;
+
+                int day = timeKeeper.CurrentDay;
+                if (day % 7 == 0)
+                {
+                    currentActivity = Activity.Fighting;
+                    turn = true;
+
+                    enemyHealth = 500 * day * 0.1f;
+                    enemyStrength = 30 * day * 0.075f;
+                }
             }
         }
 
         if (stats != null)
         {
-            stats.text =
-                $"Health: {health:f0}\n" +
-                $"Strength: {strength:f0}\n" +
-                $"Tunnel: {tunnel:f0}m";
+            if (tunnel < 1000)
+            {
+                if (health > 0)
+                {
+                    string info =
+                        $"Health: {health:f0}\n" +
+                        $"Strength: {strength:f0}\n" +
+                        $"Tunnel: {tunnel:f0}m";
+
+                    if (currentActivity == Activity.Fighting)
+                        info += $"\n" +
+                            $"EnemyHealth: {enemyHealth:f0}\n" +
+                            $"EnemyStrength: {enemyStrength:f0}";
+
+                    stats.text = info;
+                }
+                else
+                    stats.text = "You Lose";
+            }
+            else
+                stats.text = "You Win";
         }
 
         if (display != null && (int)currentActivity < sprites.Length)
@@ -97,7 +161,7 @@ public class StatsKeeper : MonoBehaviour
 
     public void ChangeActivity(Activity activity)
     {
-        if (currentActivity != Activity.Sleeping)
+        if (currentActivity != Activity.Sleeping && currentActivity != Activity.Fighting)
         {
             currentActivity = activity;
             timeKeeper.Stopped = activity == Activity.Idle;
@@ -111,6 +175,8 @@ public class StatsKeeper : MonoBehaviour
 
     public void SkipDay()
     {
+        health += 50;
+
         currentActivity = Activity.Sleeping;
         timeKeeper.TimePerSecond = TIME_SPEED_NIGHT;
         timeKeeper.Stopped = false;
